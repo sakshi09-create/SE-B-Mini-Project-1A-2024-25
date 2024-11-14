f2e5142c3a54d00c1e1cc000adafdabe653d59d2
shaily-62 <shailygupta0602@gmail.com>
2024-11-14 17:45:35 +0530

    Grp 10

---
+++ Book Rental System/src/book/rental/system/BookRentalSystem.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book.rental.system;
+
+/**
+ *
+ * @author Shaily Gupta
+ */
+public class BookRentalSystem {
+}
